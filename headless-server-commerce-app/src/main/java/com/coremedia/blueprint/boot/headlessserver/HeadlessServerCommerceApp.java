d5f1a50e7c8401d14ec903f954f4f49f4e37f47f
Ulrich Wilken-Keeve <uwilken@coremedia.com>
2023-11-09 12:07:27 +0100

    simplify exception handling

--- headless-server-commerce-app/src/main/java/com/coremedia/blueprint/boot/headlessserver/HeadlessServerCommerceApp.java
+++ headless-server-commerce-app/src/main/java/com/coremedia/blueprint/boot/headlessserver/HeadlessServerCommerceApp.java
@@ -4,7 +4,6 @@ import com.coremedia.caas.richtext.stax.config.InvalidDefinition;
 import com.coremedia.util.Hooks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -33,15 +32,13 @@ public class HeadlessServerCommerceApp {
     try {
       Hooks.enable();
       SpringApplication.run(HeadlessServerCommerceApp.class, args);
-    } catch (BeanCreationException e) {
+    } catch (Exception e) {
       Throwable c = unwrap(e);
       if (InvalidDefinition.class.isAssignableFrom(c.getClass())) {
         LOG.error("Application startup failed, cause: {}", ((InvalidDefinition) c).getDetailMessage());
-        return;
+      } else {
+        LOG.error("Application startup failed, cause: {}", e.getMessage());
       }
-      LOG.error("Application startup failed, cause: {}", e.getMessage());
-    } catch (Exception e) {
-      LOG.error("Application startup failed, cause: {}", e.getMessage());
     } finally {
       Hooks.disable();
     }
